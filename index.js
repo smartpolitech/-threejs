29cb58d1d852069ddb27d97a7db865606175345d
kathyer <joseluis_f1@hotmail.com>
2017-09-15 11:39:56 +0200

    Reestructuración
    Se han eliminado carpetas que no están en el directorio correcto

--- index.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-
-var app = require("./app");
-var puerto = process.env.PORT || 3977;
-
-app.listen(puerto, function()
-	{
-		console.log("Servidor del api rest para la lectura de datos funcionando en http://localhost:" + puerto);
-	});
--- public/modelos/AIN/P01/datos.js
+++
@@ -1,7 +0,0 @@
-{
-"Edificio": [
-	{ "nombreFichero":"BASE.js" , "habitacion":"BASE", "edificio":"AIN", "planta":"P01" },
-	{ "nombreFichero":"COM001.js" , "habitacion":"COM001", "edificio":"AIN", "planta":"P01" },
-	{ "nombreFichero":"CUA002.js" , "habitacion":"CUA002", "edificio":"AIN", "planta":"P01" }
-]
-}
